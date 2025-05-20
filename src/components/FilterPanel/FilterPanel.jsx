f0e8af81c432b11e67d5c0e8d7a2f5611d601e67
Oleg <sleite4r@gmail.com>
2025-05-20 22:47:59 +0200

    refactor: remove Formik and implement manual filtering logic in FilterPanel

--- src/components/FilterPanel/FilterPanel.jsx
+++ src/components/FilterPanel/FilterPanel.jsx
@@ -1,37 +1,86 @@
-import React, { useContext } from "react";
-import { Formik, Form, Field } from "formik";
+//FilterPanel.jsx
 
-import ProductList from "../ProductList/ProductList";
-import { ProductContext } from "../../ProductsContext/ProductsContext";
+import React, { useEffect, useState } from "react";
 
-export const FilterPanel = () => {
-  const { products } = useContext(ProductContext);
+// const { products } = useContext(ProductContext);
+const FilterPanel = ({ products, onFilter }) => {
+  const [searchTerm, setSearchTerm] = useState("");
+  const [sortBy, setsortBy] = useState("");
+  const [category, setcategory] = useState("all");
+
+  useEffect(() => {
+    let filtered = [...products];
+
+    if (searchTerm.trim() !== "") {
+      let search = searchTerm.toLowerCase();
+
+      filtered = filtered.filter(
+        (p) =>
+          p.title.toLowerCase().includes(search) ||
+          p.description.toLowerCase().includes(search)
+      );
+    }
+
+    if (category !== "all") {
+      filtered = filtered.filter((product) => product.category === category);
+    }
+
+    if (sortBy === "title") {
+      filtered.sort((a, b) => a.title.localeCompare(b.title));
+    } else if (sortBy === "price") {
+      filtered.sort((a, b) => a.price - b.price);
+    } else if (sortBy === "rating") {
+      filtered.sort((a, b) => b.rating - a.rating);
+    }
+
+    onFilter(filtered);
+  }, [searchTerm, sortBy, category, products, onFilter]);
+
   return (
-    <Formik
-      initialValues={{ searchTerm: "", sortBy: "", category: "all" }}
-      onSubmit={() => {}}
-    >
-      {({ values }) => {
-        let filtered = [...products];
+    <div>
+      <div>
+        <form>
+          <div>
+            <label htmlFor="searchTherm">Search for Products: </label>
+            <input
+              type="text"
+              id="searchTerm"
+              value={searchTerm}
+              onChange={(e) => setSearchTerm(e.target.value)}
+            />
+          </div>
 
-        if (values.searchTerm.trim() !== "") {
-          let search = values.searchTerm.toLowerCase();
-
-          filtered = filtered.filter(
-            (p) =>
-              p.title.toLowerCase().includes(search) ||
-              p.description.toLowerCase().includes(search)
-          );
-        }
-
-        if (values.sortBy === "title") {
-          filtered.sort((a, b) => a.title.localeCopare(b.title));
-        } else if (values.sortBy === "price") {
-          filtered.sort((a, b) => a.price - b.price);
-        } else if (values.sortBy === "rating") {
-          filtered.sort((a, b) => b.rating - a.rating);
-        }
-      }}
-    </Formik>
+          <div>
+            <label htmlFor="sortBy">Sort By:</label>
+            <select
+              id="sortBy"
+              value={sortBy}
+              onChange={(e) => setsortBy(e.target.value)}
+            >
+              <option value="">None</option>
+              <option value="title">Product (A-Z)</option>
+              <option value="price">Price</option>
+              <option value="rating">Rating</option>
+            </select>
+          </div>
+          <div className="">
+            <label htmlFor="category">Category: </label>
+            <select
+              id="category"
+              value={category}
+              onChange={(e) => setcategory(e.target.value)}
+            >
+              <option value="all">All</option>
+              <option value="beauty">Иeauty</option>
+              <option value="fragrances">Fragrances</option>
+              <option value="furniture">Furniture</option>
+              <option value="groceries">Groceries</option>
+            </select>
+          </div>
+        </form>
+      </div>
+    </div>
   );
 };
+
+export default FilterPanel;
--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -1,25 +1,35 @@
 // ProductList.jsx
 
-import React from "react";
+import React, { useContext, useState, useEffect } from "react";
+import FilterPanel from "../FilterPanel/FilterPanel";
 import ProductCard from "../ProductCard/ProductCard";
 import { Pagination } from "../Pagination/Pagination";
+import { ProductContext } from "../../ProductsContext/ProductsContext";
 
-const ProductList = ({ products, page, totalPages, onPageChange }) => {
+const ProductList = () => {
+  const { products, page, totalPages, onPageChange } =
+    useContext(ProductContext);
+  const [filteredProducts, setFilteredProducts] = useState(products);
+
+  useEffect(() => {
+    setFilteredProducts(products);
+  }, [products]);
+
   return (
     <section>
+      <h2>Products</h2>
+      <FilterPanel products={products} onFilter={setFilteredProducts} />
       <div>
-        <h2>Products</h2>
-        <div>
-          {products.map((p) => (
-            <ProductCard key={p.id} products={p} />
-          ))}
-        </div>
-        <Pagination
-          page={page}
-          totalPages={totalPages}
-          onPageChange={onPageChange}
-        />
+        {(filteredProducts.length ? filteredProducts : products).map((p) => (
+          <ProductCard key={p.id} products={p} />
+        ))}
       </div>
+
+      <Pagination
+        page={page}
+        totalPages={totalPages}
+        onPageChange={onPageChange}
+      />
     </section>
   );
 };
