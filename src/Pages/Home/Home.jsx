050e84621c80c4678cd90f7d947c7d35414fb93e
Oleg <sleite4r@gmail.com>
2025-05-21 22:27:07 +0200

    style hero section

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -3,10 +3,14 @@ import styles from "./Home.module.css";
 
 const Home = () => {
   return (
-    <section>
+    <section className={styles.homeSection}>
       <div className={`${styles.homeContainer} container`}>
-        <div>
-          <h1 className={ styles.homeTitle}>Home</h1>
+        <div className={styles.homeTitleContainer}>
+          {/* <h1 className={styles.homeTitle}>Home</h1> */}
+          <h1 className={styles.homeTitle}>
+            Big Deals in a Mini Shop – Up to 20% Off!
+          </h1>
+          <p className={styles.heroSubtitle }>Discover beauty, gadgets, and more — all in one stylish place.</p>
         </div>
       </div>
     </section>
